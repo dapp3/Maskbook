e75b17dcab2c7388844336f2ead6d6bec858b8ac
guanbinrui <guanbinrui@dimension.im>
2022-04-08 15:25:21 +0800

    chore: wip

--- packages/plugins/EVM/src/state/index.ts
+++ packages/plugins/EVM/src/state/index.ts
@@ -1,11 +1,44 @@
-export * from './AddressBook'
-export * from './Asset'
-export * from './Account'
-export * from './Token'
-export * from './TokenList'
-export * from './Transaction'
-export * from './NameService'
-export * from './Protocol'
-export * from './Wallet'
-export * from './Shared'
-export * from './Utils'
+import type { RequestArguments, TransactionConfig } from 'web3-core'
+import type { Plugin, Web3Plugin } from '@masknet/plugin-infra'
+import type { EthereumTransactionConfig } from '@masknet/web3-shared-evm'
+import {AddressBookState} from './AddressBook'
+import {AssetState} from './Asset'
+import {AccountState} from './Account'
+import {TokenState} from './Token'
+import {TokenListState} from './TokenList'
+import {TransactionState} from './Transaction'
+import {NameServiceState} from './NameService'
+import {ProtocolState} from './Protocol'
+import {WalletState} from './Wallet'
+import {SharedState} from './Shared'
+import {UtilState} from './Utils'
+
+export type State = Web3Plugin.ObjectCapabilities.Capabilities<EthereumTransactionConfig, RequestArguments, TransactionConfig>
+
+let state: State | null = null
+
+export async function setupState(context: Plugin.SNSAdaptor.SNSAdaptorContext) {
+    state = {
+        Account: new AccountState(),
+        AddressBook: new AddressBookState(),
+        Asset: new AssetState(),
+        NameService: new NameServiceState(),
+        Token: new TokenState(),
+        TokenList: new TokenListState(),
+        Transaction: new TransactionState(),
+        Protocol: new ProtocolState(context),
+        Wallet: new WalletState(context),
+        Shared: await new SharedState().create(),
+        Utils: await new UtilState().create(),
+    }
+    return state
+}
+
+export function getState() {
+    if (!state) throw new Error('Please setup state at first.')
+    return state
+}
+
+export async function setState(newState: State) {
+    state = newState
+}
